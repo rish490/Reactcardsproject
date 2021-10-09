1215fca2c331ec047956ceb973f8366142c23424
Rishabh Prasanna <88023315+rish490@users.noreply.github.com>
2021-10-09 19:59:48 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4E03AQE0cXZMdXckqQ/profile-displayphoto-shrink_800_800/0/1619628049379?e=1639008000&v=beta&t=H7xBbqfR28Q-FxlE-Ijip1Sm1EdmNrHLPpbnkOfjBvM" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//this is just a little more props practice file
+//analyze properly to get full idea
